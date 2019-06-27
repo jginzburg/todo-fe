6b19a6d98d384163d05b7315df541e819e4bd58f
Jeronimo Ginzburg <jginzburg@redhat.com>
2019-06-27 14:25:22 -0300

    Implementing e2e tests

--- tests/e2e/specs/importantFlag.js
+++ tests/e2e/specs/importantFlag.js
@@ -2,7 +2,7 @@
 	'Testing important flag setting': browser => {
 	 browser
 		.url(process.env.VUE_DEV_SERVER_URL + '/#/todo')
-		.waitForElementVisible('body', 5000);
+		.waitForElementVisible('body', 5000)
 		.pause(5000)
     		.click('#clear-all')
 		.pause(2000)
